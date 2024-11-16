2c978a72be953375090c5925ba84654725c2485a
abhigna <abhigna@abhigna.>
2024-11-16 14:49:57 +0000

    added changes

--- shell-scripting/create_ec2.sh
+++ shell-scripting/create_ec2.sh
@@ -26,8 +26,60 @@ aws --version
 
 }
 
+
+create_ec2() {
+
+local ami_id  = $1
+local instance_type = $2
+local key_name = $3
+local security_group = $4
+local subnet_id = $5 
+
+instanceid(aws ec2 run-instances \
+    --image-id = $ami_id \
+    --instance-type = $instance_type \
+    --key-name = $key_name \
+    --security-group-ids $security_group \
+    --subnet-id $subnet_id \))
+
+}
+
+wait_for_running() {
+
+local $instanceid = "$1"
+
+while true; do 
+
+state = $(aws ec2 describe-instances --instance-ids "$instanceid" --query 'Reservations[0].Instances[0].State.Name' --output text)
+
+if [["$state" == "Running"]]; then
+
+echo "instance "$instanceid" is in running state"
+
+break
+
+fi
+
+sleep 10
+
+done
+
+}
+
+parameters() {
+
+"$ami_id" = 
+"$instance_type" =
+"$key_name" = 
+"$security_group" =
+"$subnet_id" =
+
+}
+
 check_awscli
 
 install_aws_cli
 
+create_ec2
 
+wait_for_running
